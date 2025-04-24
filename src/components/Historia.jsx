872c21df9f3c81c5b61b4ef4765c41c46c7c2f2d
Fran Uncal <franuncal@gmail.com>
2025-04-23 22:42:16 -0300

    nuevos eventos

--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import "./Historia.css";
-
-// const Historia = ({ historia }) => {
-//   return (
-//     <section
-//       id="historia"
-//       className="historia"
-//       style={{
-//         backgroundColor: historia.colorSecundario,
-//       }}
-//     >
-//       <div className="historia-texto">
-//         <h2 className="historia-titulo">
-//           <span class="resaltado">Nuestra</span> Historia
-//         </h2>
-//         <p className="historia-descripcion">{historia.descripcion}</p>
-//       </div>
-//       <div className="historia-imagen-contenedor">
-//         <img
-//           src={historia.imagen}
-//           alt="Historia de la Fiesta"
-//           className="historia-imagen"
-//         />
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Historia;
-
 import React from "react";
 import "./Historia.css";
 
